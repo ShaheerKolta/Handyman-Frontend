a9b1731ab23223e0d10996efa93bac551bc17f7a
Sara <kokesa20@yahoo.com>
2022-08-06 16:18:23 +0200

    Adjust Appliances Technician

--- src/app/shared/data/products.ts
+++ src/app/shared/data/products.ts
@@ -1,113 +1,113 @@
 export class productsDB {
-    static Product = [
-        {
-            id: 1,
-            created_by: {
-                name: 'Plumbers',
-                avatar: 'assets/images/avatar.png'
-            },
-            images: ['assets/images/craftsnames/Plumbers.png', 'assets/images/products/02-2.png', 'assets/images/products/02-3.png'],
-            name: 'Choose your handyman',
-            price: 3000.00,
-            rating: 4.8,
-            feedback: 3,
-            category: 'digitalization',
-            tags: [
-                'Diesel',
-                'Hudson',
-                'Lee'
-            ],
-        },
-        {
-            id: 2,
-            created_by: {
-                name: 'Carpenters',
-                avatar: 'assets/images/avatar.png'
-            },
-            images: ['assets/images/craftsnames/Carpenters.png', 'assets/images/products/01-2.png', 'assets/images/products/01-3.png'],
-            name: 'Choose your handyman',
-            price: 3000.00,
-            rating: 4.8,
-            feedback: 3,
-            category: 'digitalization',
-            tags: [
-                'Diesel',
-                'Hudson',
-                'Lee'
-            ],
-        },
-        {
-            id: 3,
-            created_by: {
-                name: 'Mechanics',
-                avatar: 'assets/images/avatar.png'
-            },
-            images: ['assets/images/craftsnames/Mechanics.png', 'assets/images/products/01-2.png', 'assets/images/products/01-3.png'],
-            name: 'Choose your handyman',
-            price: 3000.00,
-            rating: 4.8,
-            feedback: 3,
-            category: 'digitalization',
-            tags: [
-                'Diesel',
-                'Hudson',
-                'Lee'
-            ],
-        },
-        {
-            id: 4,
-            created_by: {
-                name: 'Painters',
-                avatar: 'assets/images/avatar.png'
-            },
-            images: ['assets/images/craftsnames/Painters.png', 'assets/images/products/02-2.png', 'assets/images/products/02-3.png'],
-            name: 'Choose your handyman',
-            price: 3000.00,
-            rating: 4.8,
-            feedback: 3,
-            category: 'digitalization',
-            tags: [
-                'Diesel',
-                'Hudson',
-                'Lee'
-            ],
-        },
-        {
-            id: 5,
-            created_by: {
-                name: 'Electricians',
-                avatar: 'assets/images/avatar.png'
-            },
-            images: ['assets/images/craftsnames/Electricians.png', 'assets/images/products/01-2.png', 'assets/images/products/01-3.png'],
-            name: 'Choose your handyman',
-            price: 3000.00,
-            rating: 4.8,
-            feedback: 3,
-            category: 'digitalization',
-            tags: [
-                'Diesel',
-                'Hudson',
-                'Lee'
-            ],
-        },
-        {
-            id: 6,
-            created_by: {
-                name: 'Appliances technician',
-                avatar: 'assets/images/avatar.png'
-            },
-            images: ['assets/images/craftsnames/AppliancesTechnicians.png', 'assets/images/products/02-2.png', 'assets/images/products/02-3.png'],
-            name: 'Choose your handyman',
-            price: 3000.00,
-            rating: 4.8,
-            feedback: 3,
-            category: 'digitalization',
-            tags: [
-                'Diesel',
-                'Hudson',
-                'Lee'
-            ],
-        }/* ,
+  static Product = [
+    {
+      id: 1,
+      created_by: {
+        name: 'Plumbers',
+        avatar: 'assets/images/avatar.png'
+      },
+      images: [
+        'assets/images/craftsnames/Plumbers.png',
+        'assets/images/products/02-2.png',
+        'assets/images/products/02-3.png'
+      ],
+      name: 'Choose your handyman',
+      price: 3000.0,
+      rating: 4.8,
+      feedback: 3,
+      category: 'digitalization',
+      tags: ['Diesel', 'Hudson', 'Lee']
+    },
+    {
+      id: 2,
+      created_by: {
+        name: 'Carpenters',
+        avatar: 'assets/images/avatar.png'
+      },
+      images: [
+        'assets/images/craftsnames/Carpenters.png',
+        'assets/images/products/01-2.png',
+        'assets/images/products/01-3.png'
+      ],
+      name: 'Choose your handyman',
+      price: 3000.0,
+      rating: 4.8,
+      feedback: 3,
+      category: 'digitalization',
+      tags: ['Diesel', 'Hudson', 'Lee']
+    },
+    {
+      id: 3,
+      created_by: {
+        name: 'Mechanics',
+        avatar: 'assets/images/avatar.png'
+      },
+      images: [
+        'assets/images/craftsnames/Mechanics.png',
+        'assets/images/products/01-2.png',
+        'assets/images/products/01-3.png'
+      ],
+      name: 'Choose your handyman',
+      price: 3000.0,
+      rating: 4.8,
+      feedback: 3,
+      category: 'digitalization',
+      tags: ['Diesel', 'Hudson', 'Lee']
+    },
+    {
+      id: 4,
+      created_by: {
+        name: 'Painters',
+        avatar: 'assets/images/avatar.png'
+      },
+      images: [
+        'assets/images/craftsnames/Painters.png',
+        'assets/images/products/02-2.png',
+        'assets/images/products/02-3.png'
+      ],
+      name: 'Choose your handyman',
+      price: 3000.0,
+      rating: 4.8,
+      feedback: 3,
+      category: 'digitalization',
+      tags: ['Diesel', 'Hudson', 'Lee']
+    },
+    {
+      id: 5,
+      created_by: {
+        name: 'Electricians',
+        avatar: 'assets/images/avatar.png'
+      },
+      images: [
+        'assets/images/craftsnames/Electricians.png',
+        'assets/images/products/01-2.png',
+        'assets/images/products/01-3.png'
+      ],
+      name: 'Choose your handyman',
+      price: 3000.0,
+      rating: 4.8,
+      feedback: 3,
+      category: 'digitalization',
+      tags: ['Diesel', 'Hudson', 'Lee']
+    },
+    {
+      id: 6,
+      created_by: {
+        name: 'Appliances Technician ',
+        avatar: 'assets/images/avatar.png'
+      },
+      images: [
+        'assets/images/craftsnames/AppliancesTechnicians.png',
+        'assets/images/products/02-2.png',
+        'assets/images/products/02-3.png'
+      ],
+      name: 'Choose your handyman',
+      price: 3000.0,
+      rating: 4.8,
+      feedback: 3,
+      category: 'digitalization',
+      tags: ['Diesel', 'Hudson', 'Lee']
+    } /* ,
         {
             id: 7,
             created_by: {
@@ -125,7 +125,7 @@ export class productsDB {
                 'Hudson',
                 'Lee'
             ],
-        }, *//* 
+        }, */ /* 
         {
             id: 8,
             created_by: {
@@ -162,5 +162,5 @@ export class productsDB {
                 'Lee'
             ],
         }, */
-    ]
+  ];
 }
